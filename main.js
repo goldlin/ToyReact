50a67d94285cda6402b46ebabd6b2a586fc12ebd
Gordon Lin <gordonlin@qeeka.com>
2020-09-08 19:15:05 +0800

    implement setState

--- main.js
+++ main.js
@@ -11,7 +11,9 @@ class MyComponent extends Component {
   render() {
     return <div>
       <h1>my component</h1>
+      <button onclick={() => { this.setState({ a: this.state.a + 1 }) }}>add</button>
       <span>{this.state.a.toString()}</span>
+      <span>{this.state.b.toString()}</span>
       {this.children}
     </div>
   }
